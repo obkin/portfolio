d575cbe51c4f771425177ea0531c4285eb596944
mac <mac@MacBook-Air-mac.local>
2022-11-02 20:47:19 +0200

    script.js: logo-fixed

--- portfolio-main-page/src/js/script.js
+++ portfolio-main-page/src/js/script.js
@@ -1,7 +1,64 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    const logoBg = document.querySelector('#logo-bg'),
+          logoText = document.querySelector('#logo-text');
+
+    let logoAnim;
+
+    function anim() {
+        logoAnim = logoBg.animate([
+            {
+                transform: 'translateX(-50%) rotate(0deg)',
+                left: '50%'
+            },
+            {
+                transform: 'translateX(-50%) rotate(180deg)',
+                left: '50%'
+            },
+            {
+                transform: 'translateX(-50%) rotate(270deg)',
+                left: '50%'
+            },
+            {
+                transform: 'translateX(-50%) rotate(360deg)',
+                left: '50%'
+            }
+        ], {
+            duration: 3000,
+            iterations: 1
+        });
+        setTimeout(() => {
+            window.location.reload();
+        }, 1000);
+        logoAnim = 0;
+    }
+
+    logoBg.addEventListener('click', () => {
+        anim();
+    });
+
+    logoText.addEventListener('click', () => {
+        anim();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Logo Animation
 
+/*
     const logoBg = document.querySelector('#logo-bg');
 
     let logoAnim;
@@ -35,5 +92,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 1000);
         logoAnim = 0;
     });
+    
+*/
 
-});
