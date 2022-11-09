17fbb2e5755bb1053420b353ca90171d3386c923
obkin <kapellwork@gmail.com>
2022-11-09 15:51:12 +0200

    slider: stage 1

--- portfolio-main-page/src/js/script.js
+++ portfolio-main-page/src/js/script.js
@@ -45,6 +45,27 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // Slider
 
+    function slider(rightArrowSelector, leftArrowSelector, slidesSelector, wrapperSelector, slidesInnerSelector) {
+        
+        const rightArrow = document.querySelector(rightArrowSelector),
+              leftArrow = document.querySelector(leftArrowSelector),
 
+              slidesWrapper = document.querySelector(wrapperSelector),
+              slides = slidesWrapper.querySelectorAll(slidesSelector),
+              slidesInner = slidesWrapper.querySelector(slidesInnerSelector),
+              width = window.getComputedStyle(slidesInner).width; 
+
+        let slideIndex = 1;
+
+        slidesInner.style.width = 100 * slides.length + '%';
+        slidesInner.style.display = 'flex';
+        slidesInner.style.transition = '0.5s all';
+
+        slides.forEach(slide => {
+            slide.style.width = width;
+        });
+    }
+
+    slider('.welcome_slider-bg-circle-right', '.welcome_slider-bg-circle-left', '.welcome_slider-slide', '.welcome_slider-wrapper', '.welcome_slider-inner');
 
 });
